6f61acba619528e12e4e60d67ab10fd0c5539a94
sergimichi <sergimichi@users.noreply.github.com>
2026-05-26 17:21:28 +0200

    chore: bump version to 0.0.5 for PyPI release

--- vectorzip/__init__.py
+++ vectorzip/__init__.py
@@ -1,5 +1,5 @@
 # VectorZip: High-performance RAG vector compression
 from .compressor import VectorZip, VectorZipModel, compress
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __all__ = ["VectorZip", "VectorZipModel", "compress"]
